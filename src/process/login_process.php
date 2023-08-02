fad3fe270e4543a2f6f4cca6c7975d7e0fe56b7b
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2023-08-02 23:18:20 +0530

    create process php scripts

---
+++ src/process/login_process.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once "src/classes/DBConnector.php";
+use Elezione\classes\DBConnector;
+
+$con = DBConnector::getConnection();
+?>
+
+<!DOCTYPE html>
+<head><title>Login</title></head>
+<body>
+<?php
+//example code for you
+$query = "select * from user";
+try {
+    $stmt = $con->query($query);
+    $rs = $stmt->fetchAll(PDO::FETCH_OBJ);
+    foreach ($rs as $row) {
+        echo "success";
+    }
+    if(empty($rs)){
+        echo "empty";
+    }
+}catch (PDOException $ex){
+    die("Error : ".$ex->getMessage());
+}
+?>
+</body>
